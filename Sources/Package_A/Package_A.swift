b992e1fb1010f9b089edd9df03f927663c35fd49
Chris Marshall <chris@riftvalleysoftware.com>
2020-07-01 17:31:25 -0400

    Added support for an indent property.

--- Sources/Package_A/Package_A.swift
+++ Sources/Package_A/Package_A.swift
@@ -2,8 +2,15 @@ import Foundation
 
 protocol PackageProtocol {
     var text: String { get }
+    var indent: Int { get }
 }
 
 struct Package_A: PackageProtocol {
-    let text = "Package_A, Version: 1.0.0"
+    let indent: Int
+    let text: String
+    init(indent inIndent: Int = 0) {
+        indent = inIndent
+        let prefix = String(repeating: "\t", count: inIndent)
+        text =  "\(prefix)Package_A, Version: 1.0.0"
+    }
 }
