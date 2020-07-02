9812f75e57dd22291109d0d4b478d521a4072df7
Chris Marshall <chris@riftvalleysoftware.com>
2020-07-01 20:24:49 -0400

    Dammit. I had to make the initializer public, too.

--- Sources/Package_A/Package_A.swift
+++ Sources/Package_A/Package_A.swift
@@ -8,7 +8,7 @@ public protocol PackageProtocol {
 public struct Package_A: PackageProtocol {
     public let indent: Int
     public let text: String
-    init(indent inIndent: Int = 0) {
+    public init(indent inIndent: Int = 0) {
         indent = inIndent
         let prefix = String(repeating: "\t", count: inIndent)
         text =  "\(prefix)Package_A, Version: 1.0.0"
